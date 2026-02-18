2bae1dc8c881023dde2c73082fbbc0b024bf86fa
PanteraCorRosa <121111305+PanteraCorRosa@users.noreply.github.com>
2026-02-18 15:05:48 -0300

    Update obras.js

--- assets/js/obras.js
+++ assets/js/obras.js
@@ -1,1 +1,22 @@
+const obras = [
+  {
+    id: 1,
+    titulo: "Obra Exemplo",
+    descricao: "Uma história intensa cheia de mistério.",
+  }
+];
 
+const container = document.querySelector(".container");
+
+obras.forEach(obra => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  card.innerHTML = `
+    <h3>${obra.titulo}</h3>
+    <p>${obra.descricao}</p>
+    <a href="#" class="botao">Ler Agora</a>
+  `;
+
+  container.appendChild(card);
+});
